215c709a188f30db8a9165747ede68a62701684e
Arcaytus <manu.martinezgalan@gmail.com>
2021-09-01 15:53:58 -0300

    replaced previous components

---
+++ src/api/config.js
@@ -0,0 +1,2 @@
+export const API_KEY= '1a5818f374677bcbc46deb6abb8cbaea';
+export const API_BASE_URL= 'http://api.openweathermap.org/';
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,25 +1,31 @@
 import React from "react";
-
-
-
-export default class Forecast extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-  
+import moment from "moment";
+class Forecast extends React.Component {
     render() {
-      const { day } = this.props;
-      if (!day) return null;
-      return (
-        <div className="forecast-container">
-          <div className="image">
-            <img src={day.condition.icon} />
-          </div>
-          <div className="text">{day.avgtemp_c}</div>
-          <div className="muted-text">{day.condition.text}</div>
+        const forecast = this.props.forecasts; 
+        return (
+        <React.Fragment>
+        <h3 className="Result-info">Next 5 day weather forecast at 06:00 AM</h3>
+        <div className="Cards-next-five">
+            { forecast.list.filter(day => day.dt_txt.endsWith("06:00:00")).map((day, index) => {
+                return (
+                <div className="Card-next-five" key={index}>
+                  <p className="Date-time">{moment(day.dt_txt).format("ddd, DD MMM YY - HH:mm A")}</p>
+                  <p className="Weather-desc">{day.weather[0].description}</p>
+                  <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} alt={`weather-icon-${day.weather[0].description}`} />
+                  <p className="Temperature-cond">{Math.round(day.main.temp)} &#x2103; | {Math.round((day.main.temp * 9 / 5) + 32)} &#x2109;</p>
+                  <p className="Humid">{}</p>
+                </div>
+                );
+            })
+            }
         </div>
-      );
+        </React.Fragment>
+        );
     }
-  }
+}
+
+
+
 
 export default Forecast;
