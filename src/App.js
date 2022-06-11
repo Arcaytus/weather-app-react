192d0916f4a15898d42b9965bd906c42455a33f0
Manu-Martinez <manu.martinezgalan@gmail.com>
2022-06-11 01:42:21 -0300

    made a simpler and updated app

--- src/App.js
+++ src/App.js
@@ -1,37 +1,65 @@
-import React from "react";
-import CitySelector from "./components/CitySelector";
-import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-import { Container } from "react-bootstrap";
-import UseFetch from "./hooks/UseFetch";
-import { API_KEY, API_BASE_URL } from "./api/config";
-import WeatherList from "./components/WeatherList";
 
 const App = () => {
-  // destructure the returned values
-  const { data, error, isLoading, setUrl } = UseFetch();
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=2084087942391e9ed88c835fcc966d0b`;
 
-  // error handling
-  const getContent = () => {
-    if (error) return <h2>Error when fetching: {error}</h2>;
-    if (!data && isLoading) return <h2>LOADING...</h2>;
-    if (!data) return null;
-    return <WeatherList weathers={data.list} />;
-  };
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+      });
+      setLocation('')
+    }
+  }
 
   return (
-    <Container>
-      <div className="App">
-        <CitySelector
-          onSearch={(city) =>
-            setUrl(
-              `${API_BASE_URL}/data/2.5/forecast?q=${city}&units=metric&cnt=10&lang=sp&appid=${API_KEY}`
-            )
-          }
-        />
-        {getContent()}
+    <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
       </div>
-    </Container>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+          </div>
+        </div>
+
+        {data.name !== undefined &&
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}°C</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+        }
+
+
+
+      </div>
+    </div>
   );
 };
 
